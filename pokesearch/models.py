89984ed47d6956d45d467949d95eb6d4ab65926d
brad-wilkes <rbwilkes@yahoo.com>
2025-06-04 12:43:59 -0500

    add meta class to pokemon

--- pokesearch/models.py
+++ pokesearch/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class Type(models.Model):
+    name = models.CharField(unique=True)
+
+class Pokemon(models.Model):
+    name = models.CharField(max_length=100)
+    type1 = models.ManyToManyField(Type, related_name="type1")
+    type2 = models.ManyToManyField(Type, related_name='type2')
+    generation = models.IntegerField()
+
+    class Meta:
+        verbose_name = "Pokemon"
+        verbose_name_plural = "Pokemon"
