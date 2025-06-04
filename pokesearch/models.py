8c2a67779618f9d603a06c88e83dd7747be88479
brad-wilkes <rbwilkes@yahoo.com>
2025-06-04 13:08:02 -0500

    fix related name

--- pokesearch/models.py
+++ pokesearch/models.py
@@ -4,10 +4,13 @@ from django.db import models
 class Type(models.Model):
     name = models.CharField(unique=True)
 
+    def __str__(self):
+        return self.name
+
 class Pokemon(models.Model):
     name = models.CharField(max_length=100)
-    type1 = models.ManyToManyField(Type, related_name="type1")
-    type2 = models.ManyToManyField(Type, related_name='type2')
+    type1 = models.ForeignKey(Type, on_delete=models.CASCADE, related_name="type1_pokemon", null=True)
+    type2 = models.ForeignKey(Type, on_delete=models.CASCADE, related_name='type2_pokemon', null=True)
     generation = models.IntegerField()
 
     class Meta:
